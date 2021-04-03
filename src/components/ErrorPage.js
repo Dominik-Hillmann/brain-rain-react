90a886386cb314224ecb24a1336b077571da03ac
Dominik-Hillmann <dominik.hillmann@gmx.de>
2021-04-03 14:48:53 +0200

    Added error page

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -3,10 +3,36 @@ import React from "react";
 // Stylesheets
 import '../css/ErrorPage.css';
 // Images
-import '../img/brain-rain-logo.png';
+import logo from '../img/brain-rain-logo.png';
 
-export default class ErrorPage extends React.Component {
+/**
+ * @class Page displayed on routes that are not implemented.
+ */
+export default class ErrorPage extends React.Component {    
+    /**
+     * Get the current URL without a route.
+     * @returns {string} The URL without a route.
+     */
+    getUrlWithoutRoute() {
+        const locationParts = window.location.href.split('/');
+        const domain = locationParts[2];
+        return `https://${domain}`;
+    }
+
     render() {
-        return <h1>Cannot find this route.</h1>;
+        console.log(this.getUrlWithoutRoute());
+        return (
+            <div id="error-wrapper">
+                <img src={logo} alt="The BRAINRAIN logo." />
+                <h1>Oops! Seems like you went the wrong way.</h1>
+                <p>
+                    You can go back to the home page 👉&nbsp;
+                    <a href={this.getUrlWithoutRoute()}>
+                        this way
+                    </a>
+                    .
+                </p>
+            </div>
+        );
     }
 }
