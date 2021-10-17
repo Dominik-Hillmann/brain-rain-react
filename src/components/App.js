ab4a1ed0a74a418fdffe2cbb82e6eea369878ce3
Dominik-Hillmann <dominik.hillmann@gmx.de>
2021-10-17 19:43:12 +0200

    Deleted WikiMaps

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,6 @@ import { Switch, Route } from 'react-router-dom';
 import LandingPage from './LandingPage.js';
 import Imprint from './Imprint.js';
 import ErrorPage from './ErrorPage.js';
-import WikiMaps from './wiki-maps/WikiMaps.js';
 
 /**
  * @class Represents the website as a whole.
@@ -16,7 +15,6 @@ export default class App extends React.Component {
             <Switch>
                 <Route path="/" component={LandingPage} exact />
                 <Route path="/imprint" component={Imprint} />
-                <Route path="/wiki-maps" component={WikiMaps} />
                 <Route component={ErrorPage} />
             </Switch>
         );
