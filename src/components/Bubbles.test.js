5863a1b10932f9166cd6369a986d213ab74d4373
Dominik-Hillmann <dominik.hillmann@gmx.de>
2021-11-13 23:27:23 +0100

    Forgot to check in bubble files concerning last commit's problem

---
+++ src/components/Bubbles.test.js
@@ -0,0 +1,12 @@
+// Libraries
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+// Components
+import Bubbles from './Bubbles.js';
+
+describe('bubbles eye catcer tests.', () => {
+    test('renders the bubbles component', () => {
+        const root = document.createElement('div');
+        ReactDOM.render(<Bubbles />, root);
+    });
+});
